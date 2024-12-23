61610912d912dad8603a1d2ede1bd343f7f92f39
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2024-12-22 22:41:37 -0600

    Layout improvements

--- src/components/EntryItem.svelte
+++ src/components/EntryItem.svelte
@@ -2,10 +2,17 @@
     export let entry;
 </script>
 
-<li class="bg-base-200 p-2 rounded">
-    <p class="text-sm text-base-content">
-        {entry.measurement.reps 
-            ? `${entry.measurement.reps} reps x ${entry.measurement.weight} lbs` 
-            : `Duration: ${entry.measurement.duration}, Distance: ${entry.measurement.distance}`}
-    </p>
+<li class="p-2 cursor-pointer hover:bg-primary">
+  <p class="text-sm text-base-content">
+      {entry.measurement.reps 
+          ? `${entry.measurement.reps} reps` 
+          : `Duration: ${entry.measurement.duration}`}
+  </p>
+</li>
+<li class="p-2 cursor-pointer hover:bg-primary">
+  <p class="text-sm text-base-content">
+      {entry.measurement.reps 
+          ? `${entry.measurement.weight} lbs` 
+          : `Distance: ${entry.measurement.distance}`}
+  </p>
 </li>
--- src/components/ExerciseHistory.svelte
+++ src/components/ExerciseHistory.svelte
@@ -1,4 +1,6 @@
 <script>
+    import EntryItem from './EntryItem.svelte'; // Import the new component
+
     export let history = [];
 
     // Group history entries by date
@@ -22,26 +24,13 @@
 </script>
 
 <div class="overflow-x-auto">
-    <div class="bg-base-300 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
         {#each Object.keys(groupedHistory) as date}
             <div class="card rounded-none">
                 <h3 class="card-title text-center">{date}</h3>
-                <ul class="grid grid-cols-2">
+                <ul class="bg-base-300 space-y-2">
                     {#each groupedHistory[date] as entry}
-                        <li class="p-2 cursor-pointer hover:bg-primary">
-                            <p class="text-sm text-base-content">
-                                {entry.measurement.reps 
-                                    ? `${entry.measurement.reps} reps` 
-                                    : `Duration: ${entry.measurement.duration}`}
-                            </p>
-                        </li>
-                        <li class="p-2 cursor-pointer hover:bg-primary">
-                            <p class="text-sm text-base-content">
-                                {entry.measurement.reps 
-                                    ? `${entry.measurement.weight} lbs` 
-                                    : `Distance: ${entry.measurement.distance}`}
-                            </p>
-                        </li>
+                        <EntryItem {entry} /> <!-- Use the EntryItem component -->
                     {/each}
                 </ul>
             </div>
