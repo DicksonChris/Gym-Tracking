f15a1160390662dd1e82454763ba61c90eaa1346
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2024-12-23 11:41:40 -0600

    Adds exercise items component

--- src/components/EntryItem.svelte
+++ src/components/EntryItem.svelte
@@ -1,18 +1,20 @@
 <script>
-    export let entry;
+	export let entry;
 </script>
 
-<li class="p-2 cursor-pointer hover:bg-primary">
-    <p class="text-sm text-base-content">
-        {entry.measurement.reps 
-            ? `${entry.measurement.reps} reps` 
-            : `Duration: ${entry.measurement.duration}`}
-    </p>
-</li>
-<li class="p-2 cursor-pointer hover:bg-primary">
-    <p class="text-sm text-base-content">
-        {entry.measurement.reps 
-            ? `${entry.measurement.weight} lbs` 
-            : `Distance: ${entry.measurement.distance}`}
-    </p>
-</li>
+<div class="flex">
+	<li class="flex-1 cursor-pointer p-2 hover:bg-primary">
+		<p class="text-sm text-base-content">
+			{entry.measurement.reps
+				? `${entry.measurement.reps} reps`
+				: `Duration: ${entry.measurement.duration}`}
+		</p>
+	</li>
+	<li class="flex-1 cursor-pointer p-2 hover:bg-primary">
+		<p class="text-sm text-base-content">
+			{entry.measurement.reps
+				? `${entry.measurement.weight} lbs`
+				: `Distance: ${entry.measurement.distance}`}
+		</p>
+	</li>
+</div>
