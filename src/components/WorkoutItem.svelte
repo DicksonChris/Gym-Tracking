8f808f190d92149f5668e8497b6986659eb7f863
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2024-12-23 11:54:18 -0600

    Fixes workout items

---
+++ src/components/WorkoutItem.svelte
@@ -0,0 +1,16 @@
+<script>
+    import ExerciseItem from './ExerciseItem.svelte';
+    
+    export let workout;
+    export let exercisesData;
+    export let historyData;
+</script>
+
+<li class="card bg-base-100 shadow-lg">
+    <h2 class="card-title">{workout.groupName}</h2>
+    <ul class="ml-4 space-y-4">
+        {#each workout.exercises as exerciseId}
+            <ExerciseItem {exerciseId} {exercisesData} {historyData} />
+        {/each}
+    </ul>
+</li>
--- src/components/WorkoutList.svelte
+++ src/components/WorkoutList.svelte
@@ -2,7 +2,8 @@
     import { onMount } from 'svelte';
     import ExerciseHistory from './ExerciseHistory.svelte'; // Import the new component
     import ExerciseItem from './ExerciseItem.svelte';
-
+    import WorkoutItem from './WorkoutItem.svelte';
+    
     export let workouts = [];
 
     let exercisesData = {};
@@ -35,13 +36,6 @@
 
 <ul class="space-y-6">
     {#each workouts as workout}
-        <li class="card bg-base-100 shadow-lg"> <!-- Replaced with DaisyUI's card class -->
-            <h2 class="card-title">{workout.groupName}</h2> <!-- Removed text-primary -->
-            <ul class="ml-4 space-y-4">
-                {#each workout.exercises as exerciseId}
-                    <ExerciseItem {exerciseId} {exercisesData} {historyData} />
-                {/each}
-            </ul>
-        </li>
+        <WorkoutItem {workout} {exercisesData} {historyData} />
     {/each}
 </ul>
